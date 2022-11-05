c44fb928fc75779741e2bb0b6f4940d20aeddb07
Ernest Haruna <ernestharuna1@gmail.com>
2022-11-05 23:34:47 +0100

    Added post route

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const app = express();
 
-require('dotenv/config');
-const api = process.env.API_URL;
+const morgan = require('morgan');
 
-app.use(express.json())
+require('dotenv/config');
+const PORT = process.env.PORT || 3000;
+const user = process.env.USER;
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+const details = {
+   slackUsername: user,
+}
 
 app.get('/', (req, res) => {
-   const products = {
-      slackUsername: 'Ernesto',
-      backend: true,
-      age: 21,
-      bio: 'We worship angular'
-   }
-   res.send(products)
+   res.send(details)
 })
 
-app.listen(3000, () => {
+app.post('/', (req, res) => {
+   const newProd = req.body;
+
+   var oprt = newProd.operation_type;
+   var int1 = newProd.x;
+   var int2 = newProd.y;
+   newProd.result;
+
+   if (oprt == "addition") {
+      newProd.result = int1 + int2;
+   } else if (oprt == "subtraction") {
+      newProd.result = int1 - int2;
+   } else if (oprt == "multiplication") {
+      newProd.result = int1 * int2;
+   }
+
+   const xyz = {
+      slackUsername: user,
+      result: newProd.result,
+      operation_type: oprt,
+   }
+
+   res.send(xyz)
+})
+
+app.listen(PORT, () => {
    console.log('Server is running');
 })
